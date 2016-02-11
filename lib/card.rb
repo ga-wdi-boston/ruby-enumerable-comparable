81284c7f8c0690687f76c6c2db54a36a45a08d16
Jeffrey Horn <jeffh@generalassemb.ly>
2016-02-11 15:54:43 -0500

    Follow along with @gaand
    This uses keyword arguments with defaults and private attributes

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,22 @@
 # A simple representation of a playing card.
 class Card
+  include Comparable
+
+  def <=>(other)
+    return suit_comparison(other) if rank_comparison(other) == 0
+    rank_comparison(other)
+  end
+
+  def suit_comparison(other)
+    SUITS.index(suit) <=> SUITS.index(other.suit)
+  end
+  private :suit_comparison
+
+  def rank_comparison(other)
+    RANKS.index(rank) <=> RANKS.index(other.rank)
+  end
+  private :rank_comparison
+
   SUITS = %w(C D H S).freeze
   RANKS = [(2..10).to_a, %w(J Q K A)].flatten
 
--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -1,3 +1,20 @@
 #
 class SteppedRange
+  include Enumerable
+
+  def each
+    current = start
+
+    while current <= stop
+      yield current
+      current += step
+    end
+  end
+
+  attr_reader :start, :stop, :step
+  private :start, :stop, :step
+
+  def initialize(start:, stop:, step: 1)
+    @start, @stop, @step = start, stop, step
+  end
 end
