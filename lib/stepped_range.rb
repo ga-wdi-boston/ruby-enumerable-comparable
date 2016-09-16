546df355563619f6458d946639bb14d364c4c826
Jeffrey Horn <jeffh@generalassemb.ly>
2016-09-16 13:30:57 -0400

    Add stepped range code

--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -1,3 +1,20 @@
 #
 class SteppedRange
+  include Enumerable
+
+  def each
+    current = start
+    while current <= stop
+      yield current
+      current += step
+    end
+  end
+
+  attr_reader :start, :stop, :step
+  private :start, :stop, :step
+  def initialize(start, stop, step)
+    @start = start
+    @stop = stop
+    @step = step
+  end
 end
