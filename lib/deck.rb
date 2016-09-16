01fc422f56c3b032f59b1db04d6703e2162e72c3
Jeffrey Horn <jeffh@generalassemb.ly>
2016-09-16 12:35:51 -0400

    Make clear the difference b/t instance var and instance

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,31 +2,31 @@ require_relative 'card'
 
 # A simple represenation of a deck of playing cards
 class Deck
-  attr_reader :deck
-  private :deck
+  attr_reader :storage
+  private :storage
 
   def initialize
-    @deck = Card::SUITS.map do |suit|
+    @storage = Card::SUITS.map do |suit|
       Card::RANKS.map { |rank| Card.new(rank, suit) }
     end.flatten
   end
 
   # swap front and back somewhere in the middle third.
   def cut
-    count = deck.length
+    count = storage.length
     random = Random.rand(count / 3)
     cut_point = (count / 3 + random)
-    deck.replace deck.slice(cut_point, count - cut_point) +
-                 deck.slice(0, cut_point)
+    storage.replace storage.slice(cut_point, count - cut_point) +
+                 storage.slice(0, cut_point)
     self
   end
 
   def draw
-    @deck.shift
+    storage.shift
   end
 
   def shuffle
-    @deck.shuffle!
+    storage.shuffle!
     self
   end
 
