a742b187564cbf79587b6c7e30758146e9cc000e
Jeffrey Horn <jeffh@generalassemb.ly>
2017-02-02 15:35:02 -0500

    Update starter code

--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -2,4 +2,11 @@
 
 #
 class SteppedRange
+  attr_reader :start, :stop, :step
+  private :start, :stop, :step
+  def initialize(start, stop, step)
+    @start = start
+    @stop = stop
+    @step = step
+  end
 end
